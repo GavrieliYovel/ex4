049cb6e83f4f359937ba9e1772b6f10a40e85714
GavrieliYovel <yovel.gavrieli@stu.shenkar.ac.il>
2022-04-17 19:53:58 +0300

    final upload

--- getform.php
+++ getform.php
@@ -6,14 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Welcome</h1>
+    
     <?php
         $un = $_GET["username"];
         $coin = $_GET["coins"];
         $ph = $_GET["phone"];
+        $t = time();
 
         if($un == "yovel gavrieli")
-            echo "<h2>" . $un . "</h2><br> <h3> New coin balance: " . $coin . "<br> New phone number: " . $ph . "</h3>";
+            echo "<h2>Day of Change:". (date("Y-m-d",$t)) . "<br><h1>Welcome, </h1><h2> " . $un . "</h2><br> <h3> New coin balance: " . $coin . "<br> New phone number: " . $ph . "</h3>";
         else
             echo "<h2>Username is not exist </h2>";
      ?>
